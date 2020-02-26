5a077df4500df6ef1fbf9612926952e922caa63d
ralph-vital <44514134+ralph-vital@users.noreply.github.com>
2020-02-26 13:48:18 -0500

    this is my first commit

---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ralph Vital'
+SITENAME = 'A pelican blog'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'English'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+#THEME = "/Users/ralphvital/Tableau/Pelican/Third/pelican-themes/nmnlist"
+
+PLUGIN_PATHS = ['/Users/ralphvital/Tableau/Pelican/Third/blog_pelican/pelican-plugins']
+PLUGINS = ['tipue_search']
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
